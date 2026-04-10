eea30cf1bfbb7aeb4561bb980e98d43a118d8c48
Vlados <hanovvladik@mail.ru>
2026-04-10 11:26:47 +0300

    Step 8: middleware logging and custom headers

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -33,6 +33,33 @@ app.MapGet("/product/{id}", (int id) => new Product(
     InStock: id % 2 == 0
 ));
 
+app.Use(async (context, next) => {
+    Console.WriteLine($"[LOG] {context.Request.Method} {context.Request.Path}");
+    await next(context);
+    Console.WriteLine($"[LOG] Ответ отправлен: {context.Response.StatusCode}");
+});
+
+app.Use(async (context, next) => {
+    context.Response.Headers.Append("X-Powered-By", "ASP.NET Core Lab27");
+    await next(context);
+});
+
+app.Use(async (context, next) => {
+    var key = context.Request.Query["key"];
+    if (key != "secret")
+    {
+        context.Response.StatusCode = 401;
+        Console.WriteLine("Похоже, на этом сайте есть проблема");
+        Console.WriteLine("http://localhost:5050/ вернул ошибку.");
+        Console.WriteLine("Код ошибки: 401 Unauthorized");
+        Console.WriteLine("- Проверьте, правильно ли вы ввели адрес веб-сайта.");
+        Console.WriteLine("[Попробовать снова]");
+        return;
+    }
+    await next(context);
+});
+
+
 app.Run();
 
 record Product(int Id, string Name, decimal Price, bool InStock);
