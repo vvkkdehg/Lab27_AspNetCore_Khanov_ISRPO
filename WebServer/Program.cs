8eb5d62d06691ddbaa84b20c428f6e544139e4bf
Vlados <hanovvladik@mail.ru>
2026-04-10 11:04:49 +0300

    Step 7: returning JSON from routes

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -11,4 +11,28 @@ app.MapGet("/hello/{name}", (string name) => $"Привет, {name}!");
 
 app.MapGet("/sum/{a}/{b}", (int a, int b) => $"{a + b}");
 
+app.MapGet("/student", () => new
+{
+    Name = "Владислав Ханов",
+    Group = "ИСП-231",
+    Year = 3,
+    IsActive = true
+});
+
+app.MapGet("/subjects", () => new[] {
+    "РПМ",
+    "РМП",
+    "ИСРПО",
+    "СП",
+});
+
+app.MapGet("/product/{id}", (int id) => new Product(
+    Id: id,
+    Name: $"Товар #{id}",
+    Price: id * 99.99m,
+    InStock: id % 2 == 0
+));
+
 app.Run();
+
+record Product(int Id, string Name, decimal Price, bool InStock);
